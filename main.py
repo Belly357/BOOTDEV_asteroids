67524697808cbbc5cd4c833ff5230e4d4326a245
Belly357 <beliar359@gmail.com>
2025-02-17 16:42:16 +0100

    Added Circleshape and player basics and added draw player to mid of screen

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # throughout this file
 import pygame # type: ignore
 from constants import *
+from player import *
 
 def __prog_init__():
     # pygame.init()
@@ -14,6 +15,7 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     game_clock = pygame.time.Clock()
     game_delta = 0
+    main_player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -23,7 +25,8 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
-        pygame.display.flip()
+        main_player.draw(screen)
+        pygame.display.flip()        
         game_delta = game_clock.tick(60) / 1000
 
 
